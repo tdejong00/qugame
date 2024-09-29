72acec787c8031e6c131dced960310406c62d73e
Tim de Jong <tdejong0212@gmail.com>
2024-09-29 21:44:45 +0200

    Add identity gate

---
+++ scenes/objects/identity_gate.gd
@@ -0,0 +1,9 @@
+## Represents an identity quantum gate, which leaves the state of the qubit unchanged.
+class_name IdentityGate extends QuantumGate
+
+
+func _init():
+    _matrix = [
+        1.0, 0.0,
+        0.0, 1.0
+    ]
