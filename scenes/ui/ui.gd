0e902430216b0ae785278b24550a4a186d5aa71c
Tim de Jong <tdejong0212@gmail.com>
2024-09-30 01:01:31 +0200

    Add ui scene

---
+++ scenes/ui/ui.gd
@@ -0,0 +1,4 @@
+## Represents the user interface (UI) layer.
+class_name UI extends CanvasLayer
+
+@onready var interaction_label: Label = $Control/MarginContainer/VBoxContainer/HBoxContainer/InteractionLabel
