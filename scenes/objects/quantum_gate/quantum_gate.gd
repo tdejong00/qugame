94024897638a97f038069f72c17812062c90d88d
Tim de Jong <tdejong0212@gmail.com>
2024-12-01 19:48:07 +0100

    Fix text mesh of all gates being changed when new gate is added

--- scenes/objects/quantum_gate/quantum_gate.gd
+++ scenes/objects/quantum_gate/quantum_gate.gd
@@ -16,6 +16,9 @@ enum Type {
 ## Resource path for creating instances of the scene.
 const SCENE: PackedScene = preload("res://scenes/objects/quantum_gate/quantum_gate.tscn")
 
+## Resource path for grey material.
+const GREY_MATERIAL: StandardMaterial3D = preload("res://assets/materials/grey.tres")
+
 ## The type of this quantum gate.
 @export var type: Type
 
@@ -23,13 +26,24 @@ const SCENE: PackedScene = preload("res://scenes/objects/quantum_gate/quantum_ga
 var _matrix: PackedVector2Array
 
 ## The mesh instance holding a TextMesh.
-@onready var _mesh_instance: MeshInstance3D = $Base/TextMeshInstance
+@onready var _base: Node3D = $Base
 
 
 ## Initialize text mesh and matrix
 func _ready() -> void:
-    var text_mesh: TextMesh = _mesh_instance.mesh
+    # Create text mesh representing gate
+    var mesh_instance = MeshInstance3D.new()
+    mesh_instance.position.y = 0.1
+    mesh_instance.rotation_degrees.x = -90
+    var text_mesh: TextMesh = TextMesh.new()
+    text_mesh.material = GREY_MATERIAL
+    text_mesh.font_size = 96
+    text_mesh.depth = 0.1
     text_mesh.text = type_to_string(type)
+    mesh_instance.mesh = text_mesh
+    _base.add_child(mesh_instance)
+
+    # Set correct matrix
     _matrix = type_to_matrix(type)
 
 
