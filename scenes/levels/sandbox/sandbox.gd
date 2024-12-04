1bb5c3e80167cb7166fb14bbf19dbc46372978c2
Tobias Hofstra <tobias@huizehofstra.com>
2024-12-04 15:27:26 +0100

    Level 3 done

--- scenes/levels/sandbox/sandbox.gd
+++ scenes/levels/sandbox/sandbox.gd
@@ -14,3 +14,5 @@ func _ready() -> void:
 	LevelRestrictions.allow_gate(QuantumGate.Type.PI_OVER_8)
 	LevelRestrictions.size_limit = -1
 	SignalBus.restrictions_updated.emit()
+	
+	await display("Welcome to the sandbox! Here you can use all gates, and as many as you would want!")
